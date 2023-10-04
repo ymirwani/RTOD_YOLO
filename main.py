d24a1b4093f2850288fcdf02a6d3a508827c98f8
ymirwani <72574008+ymirwani@users.noreply.github.com>
2023-10-05 01:42:41 +0200

    Update main.py
    Added other variables

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # $ pip install ultralytics
 import cv2
 from ultralytics import YOLO
-
+import math
 ## Webcam input
 
 cap = cv2.VideoCapture(0)
@@ -23,13 +23,43 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
               "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
+              "teddy bear", "hair drier", "toothbrush", "perfume"
               ]
 
 while True:
-    ret, img= cap.read()
-    cv2.imshow('Webcam', img)
+    success, img = cap.read()
+    results = model(img, stream=True)
 
+    # coordinates
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            #print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            #print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
         break
 
